6116c41a0da928f7ae14d9ff81852aef55a39c12
Truong <progger116@gmail.com>
2018-11-11 08:13:19 +0100

    init

---
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -0,0 +1,79 @@
+import React from "react";
+import {
+  Modal,
+  View,
+  Image,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const IconButton = ({ color = "black", name, size = 12, onPress, style }) => (
+  <TouchableOpacity onPress={onPress} style={style}>
+    <Icon size={size} name={name} color={color} />
+  </TouchableOpacity>
+);
+
+const placeDetail = props => {
+  let modalContent = null;
+
+  if (props.selectedPlace) {
+    modalContent = (
+      <View>
+        <Image source={props.selectedPlace.image} style={styles.placeImage} />
+        <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+      </View>
+    );
+  }
+  return (
+    <Modal
+      onRequestClose={props.onModalClosed}
+      visible={props.selectedPlace !== null}
+      animationType="slide"
+    >
+      <View style={styles.modalContainer}>
+        {modalContent}
+        <View style={styles.btn_group}>
+          {/* <Button title="Delete" color="red" onPress={props.onItemDeleted} /> */}
+          <IconButton
+            color="red"
+            name="trash"
+            size={30}
+            onPress={props.onItemDeleted}
+            style={{ marginRight: 10 }}
+          />
+
+          <IconButton
+            color="cyan"
+            name="close"
+            size={30}
+            onPress={props.onModalClosed}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    margin: 22
+  },
+  placeImage: {
+    width: "100%",
+    height: 200
+  },
+  placeName: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 28
+  },
+  btn_group: {
+    flexDirection: "row",
+    justifyContent: "center"
+  }
+});
+
+export default placeDetail;
